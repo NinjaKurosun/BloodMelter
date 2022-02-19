47d308fda0015cf3813c4f18fe7d4294722714dc
SonicTheHedgehog <97832164+Sonic-TheHedgehog@users.noreply.github.com>
2022-02-19 21:32:51 +0300

    Add files via upload

---
+++ BloodMelter/BloodMelter/BloodMelter.cpp
@@ -0,0 +1,167 @@
+#include "BloodMelter.h"
+#include <iterator>
+#include <string>
+#include <cmath>
+#include <map>
+
+#define GWL_HWNDPARENT (-8)
+
+#define COUNT_BRUSH 6
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Random64 BloodMelter::random = GetTickCount64();
+const HBRUSH BloodMelter::brush[COUNT_BRUSH] =
+{
+	CreateSolidBrush(0x000000FF), CreateSolidBrush(0x000000F3),
+	CreateSolidBrush(0x000000DA), CreateSolidBrush(0x000001C0),
+	CreateSolidBrush(0x000000A8), CreateSolidBrush(0x0000008A)
+};
+
+BloodMelter::BloodMelter(const HINSTANCE& tHInstance, RECT tWindow/*= { 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN) }*/ ) 
+	: hInstance(tHInstance), window(tWindow)
+{
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void BloodMelter::run()
+{
+	static std::wstring blob = L"Blob";
+	static std::wstring blood = L"Blood";
+	blob.push_back(L'plus');
+	blood.push_back(L'plus');
+
+	WNDCLASS wndClass = { 0, BlobProc,  0,0, hInstance, 0, 0, 0, 0, blob.c_str() };
+	WNDCLASS wndClass1 = { 0, BloodProc, 0,0, hInstance, 0, 0, 0, 0, blood.c_str() };
+	CreateMaskWindow(wndClass, hInstance, window);
+	CreateMaskWindow(wndClass1, hInstance, window);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+HWND BloodMelter::CreateMaskWindow(const WNDCLASS& wndClass, const HINSTANCE& tHInstance, const RECT& tWindow)
+{
+	if (RegisterClass(&wndClass))
+	{
+		const int posX = tWindow.left;
+		const int posY = tWindow.top;
+
+		const int Width = tWindow.right - posX;
+		const int Height = tWindow.bottom - posY;
+
+		HWND hWnd = CreateWindowEx(WS_EX_LAYERED | WS_EX_APPWINDOW | WS_EX_TOPMOST | WS_EX_TRANSPARENT, wndClass.lpszClassName,
+			0, WS_VISIBLE | WS_POPUP, posX, posY, Width, Height, HWND_DESKTOP, 0, tHInstance, 0);
+		if (hWnd)
+		{
+			SetWindowLong(hWnd, GWL_HWNDPARENT, (LONG)GetDesktopWindow());
+			SetLayeredWindowAttributes(hWnd, 0x00000000, 255, LWA_COLORKEY);
+
+			return hWnd;
+		}
+	}
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+LRESULT CALLBACK BloodMelter::BlobProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	RECT window;
+	GetWindowRect(hWnd, &window);
+
+	int Width = window.right - window.left;
+	int Height = window.bottom - window.top;
+
+	switch (Msg)
+	{
+		case WM_TIMER:
+		{
+			const uint8_t count = 4;
+
+			uint16_t posH[count];
+			uint16_t posW[count];
+			uint8_t size = random.randomFocusedOnCenter(0, 8);
+
+			const uint8_t stretch = 100;
+
+			posH[0] = random.randomFocusedOnZero(0, Height, stretch);
+			posW[0] = random.randomFocusedOnZero(0, Width, stretch);
+
+			posH[1] = random.randomFocusedOnZero(0, Height, stretch);
+			posW[1] = Width - random.randomFocusedOnZero(0, Width, stretch);
+
+			posH[2] = Height - random.randomFocusedOnZero(0, Height, stretch);
+			posW[2] = Width - random.randomFocusedOnZero(0, Width, stretch);
+
+			posH[3] = Height - random.randomFocusedOnZero(0, Height, stretch);
+			posW[3] = random.randomFocusedOnZero(0, Width, stretch);
+
+
+
+			HDC blobs = GetDC(hWnd);
+			for (int i = 0; i < count; ++i)
+			{
+				RECT rect = { posW[i], posH[i], posW[i] + size, posH[i] + size };
+				FillRect(blobs, &rect, brush[random.randomFocusedOnZero(0, COUNT_BRUSH)]);
+			}
+
+			ReleaseDC(hWnd, blobs);
+			break;
+		}
+		case WM_CLOSE:
+		{
+			return 0;
+		}
+		case WM_CREATE:
+		{
+			SetTimer(hWnd, 0, 50, 0);
+			ShowWindow(hWnd, SW_SHOW);
+			break;
+		}
+	}
+	return DefWindowProc(hWnd, Msg, wParam, lParam);
+}
+
+LRESULT CALLBACK BloodMelter::BloodProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	RECT window;
+	GetWindowRect(hWnd, &window);
+
+	const int Width = window.right - window.left;
+	const int Height = window.bottom - window.top;
+
+	switch (Msg)
+	{
+		case WM_TIMER:
+		{
+			const uint8_t sizeBlood = 18;
+
+			static std::map<uint16_t, uint16_t> blood;
+
+			HDC bloodDC = GetDC(hWnd);
+			std::map<uint16_t, uint16_t>::iterator itr;
+			for (itr = blood.begin(); itr != blood.end(); ++itr)
+			{
+				if (random(0, 6) > 3)
+				{
+					RECT melterBlood
+					{
+						itr->first, 0,
+						itr->first + random.randomFocusedOnCenter(0, sizeBlood), blood[itr->first] += random(0, sizeBlood * 2)
+					};
+					HBRUSH brush_br = brush[random(0, COUNT_BRUSH / 2)];
+					FillRect(bloodDC, &melterBlood, brush_br);
+				}
+			}
+			blood[random(0, Width)];
+			break;
+		}
+		case WM_CLOSE:
+		{
+			return 0;
+		}
+		case WM_CREATE:
+		{
+			SetTimer(hWnd, 0, 1200, 0);
+			ShowWindow(hWnd, SW_SHOW);
+			break;
+		}
+	}
+	return DefWindowProc(hWnd, Msg, wParam, lParam);
+}
---
+++ BloodMelter/BloodMelter/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <Windows.h>
+#include "BloodMelter.h"
+
+BOOL CALLBACK MonitorEnum(HMONITOR hMon, HDC hdc, LPRECT lprcMonitor, LPARAM pData)
+{
+	BloodMelter* blood = new BloodMelter((HINSTANCE)pData, *lprcMonitor);
+	blood->run();
+	return TRUE;
+}
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	//BloodMelter blood(hInstance, RECT{0,0, 1000, 100});
+	//blood.run();
+
+	EnumDisplayMonitors(0, 0, MonitorEnum, (LPARAM)hInstance);
+
+//--------------------------------------------------------------------------------------------------------------------------
+	MSG Msg;
+	while (GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return 0;
+}
